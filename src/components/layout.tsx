151d5d2c9eae3443ad9f1957fabf58676bf918c7
Tijmen Overman <tijmen.overman@jungleminds.nl>
2020-07-02 00:26:07 +0200

    Added Container component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from "react"
-import styled from "styled-components"
 
 import { StaticQuery, graphql } from "gatsby"
 
@@ -31,24 +30,15 @@ const Layout: React.FC = ({ children }) => (
       <>
         <GlobalStyle />
         <Header siteTitle={data.site.siteMetadata.title} />
-        <Container>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-        </Container>
       </>
     )}
   />
 )
-
-const Container = styled.div`
-  margin: 0 auto;
-  max-width: 960;
-  padding: 0px 1.0875rem 1.45rem;
-  padding-top: 0;
-`
 
 export default Layout
