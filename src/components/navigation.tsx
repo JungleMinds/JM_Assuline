7d1e00aa82e2e5bfd3d50d057c395f3c3bd328c8
Suheyb Coskun <suheyb.c@gmail.com>
2020-07-15 13:00:21 +0200

    Applied design feedback to header

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -94,10 +94,6 @@ export default Navigation
 
 const ContentPusher = styled.div`
   ${mediaQueries.from.breakpoint.L`
-    margin-bottom: 144px;
-  `}
-
-  ${mediaQueries.from.breakpoint.XL`
     margin-bottom: 160px;
   `}
 `
@@ -105,7 +101,6 @@ const ContentPusher = styled.div`
 const Container = styled.div<IProps>`
   position: relative;
   background: ${white};
-  margin-bottom: 16px;
   padding: 0 16px;
   max-height: 160px;
   z-index: 9;
