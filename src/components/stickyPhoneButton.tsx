24e754baa947e337997dbe37f369ace1e08ff85c
Suheyb Coskun <suheyb.c@gmail.com>
2020-07-31 10:26:23 +0200

    Added StickyPhoneButton component

---
+++ src/components/stickyPhoneButton.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// Components
+import Icon from './icons/icon'
+
+// Styles
+import colors, { green, white } from '../styles/colors'
+import mediaQueries from '../styles/mediaQueries'
+
+// Types
+interface IProps {
+  phoneNumber: string
+}
+
+const StickyPhoneButton: React.FC<IProps> = ({ phoneNumber }) => (
+  <Container href={`tel:${phoneNumber}`}>
+    <Icon icon="phone" color={white} />
+  </Container>
+)
+export default StickyPhoneButton
+
+const Container = styled.a`
+  position: fixed;
+  right: 16px;
+  bottom: 16px;
+  width: 56px;
+  height: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${green};
+  box-shadow: 0 4px 16px
+    rgba(
+      ${colors.darkest.channels.red},
+      ${colors.darkest.channels.green},
+      ${colors.darkest.channels.blue},
+      0.16
+    );
+  border-radius: 50%;
+  z-index: 1;
+
+  ${mediaQueries.from.breakpoint.S`
+      right: 20px;
+      bottom: 24px;
+  `}
+
+  ${mediaQueries.from.breakpoint.L`
+      display: none;
+  `}
+`
