34577d871bbaf6b2b515d57d598bc889790f63e3
Tijmen Overman <tijmen.overman@jungleminds.nl>
2020-08-09 21:39:01 +0200

    Added polyfill.io to support IE

--- src/components/icons/mail.tsx
+++ src/components/icons/mail.tsx
@@ -12,7 +12,6 @@ const Mail = ({ width, height, color, className }: IIconProps) => (
     color={color}
     className={className}
   >
-    >
     <g fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
       <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
       <path d="M22 6l-10 7L2 6" />
