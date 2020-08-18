5df6620a5d7f1089858373dd2e5e008d5c91fd0b
Suheyb Coskun <suheyb.c@gmail.com>
2020-08-18 10:09:56 +0200

    Applied feedback to ToastBar and CookieMessage

--- src/util/checkCookieValidity.ts
+++ src/util/checkCookieValidity.ts
@@ -1,4 +1,6 @@
-const checkCookieValidity = (cookieName: string) => {
+type CookieName = 'cookie' | 'toaster'
+
+const checkCookieValidity = (cookieName: CookieName): boolean => {
   const currentTime = new Date().getTime()
 
   const cookieStr = localStorage.getItem('assuline')
@@ -6,6 +8,15 @@ const checkCookieValidity = (cookieName: string) => {
   const validUntil = cookieObj && Number(cookieObj[cookieName])
 
   const stillValid = !!(validUntil && currentTime < validUntil)
+
+  if (!stillValid && cookieObj) {
+    if (Object.keys(cookieObj).length > 1) {
+      delete cookieObj[cookieName]
+      localStorage.setItem('assuline', JSON.stringify(cookieObj))
+    } else if (cookieObj[cookieName]) {
+      localStorage.removeItem('assuline')
+    }
+  }
 
   return stillValid
 }
